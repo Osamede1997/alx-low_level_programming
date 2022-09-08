21c7fb32b3371192f39e5189ee1ba1efdfa84f5e
Osamede1997 <derrick_rita@hotmail.com>
2022-09-08 14:42:36 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main function
+ * main function that prints out a string
+ * returns 0 as its end value
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-\n}
+}
