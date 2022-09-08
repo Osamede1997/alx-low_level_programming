a6f32dd0ab86acfc1943d9e37d7410511deb0224
Osamede1997 <derrick_rita@hotmail.com>
2022-09-08 16:29:37 -0700

    message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main-Entry point prints ith proper grammar, but the outcome is a piece of art
- * returns a value of 0 for success
+ * main- prints with proper grammar, but the outcome is a piece of art
+ * main returns a value of 0 for success.
  */
 int main(void)
 {
