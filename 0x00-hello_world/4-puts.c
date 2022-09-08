ad77193ff05b33208a9434148adccbcc9432a7d7
Osamede1997 <derrick_rita@hotmail.com>
2022-09-08 16:54:48 -0700

    message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 #include <string.h>
 
 /**
- * main-Entry point
- * description: prints string/char*
- * return value of main: Always 0(success)
+ * main- prints with proper grammar, but the outcome is a piece of art
+ * Return: 0 if exited properly and non-zero if otherwise
  */
 int main(void)
 {
