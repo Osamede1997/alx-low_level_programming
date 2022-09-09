662aa23cae9cda04099c53d86753db9321f49552
Osamede1997 <derrick_rita@hotmail.com>
2022-09-09 11:19:31 -0700

    message

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; ++x)
+{
+if ( (x != 'q') && (x != 'e')
+{
+putchar(x);
+}
+}
+putchar('\n');
+return (0);
+}
