76541c396d1e7609f70d835da183eb2ba2d1cdc5
Osamede1997 <derrick_rita@hotmail.com>
2022-09-08 17:56:06 -0700

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main- prints with proper grammar, but the outcome is a piece of art
+ * Return: 0 if exited properly and non-zero if otherwise
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
