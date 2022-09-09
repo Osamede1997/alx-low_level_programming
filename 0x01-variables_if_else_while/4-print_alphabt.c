7a213c0deb81e30c9c14bbd4155e4da8669c3bb5
Osamede1997 <derrick_rita@hotmail.com>
2022-09-09 16:27:35 -0700

    message

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,13 +2,9 @@
 #include <stdlib.h>
 
 /**
- * main entry point, it compares a random number to 5, 6, 0
- * the return value of the main function is 0 if the code runs 
- * successful, it returns a non zero value if the code 
- * isnt successful
+ * main - entry point
  * Return: 0 if exited properly and non-zero if otherwise
  */
-
 int main(void)
 {
 char x;
