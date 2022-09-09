7b42ba363f37fe941a88f60151532cceb429e391
Osamede1997 <derrick_rita@hotmail.com>
2022-09-08 18:54:38 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  * Return: 0 if exited properly and non-zero if otherwise
  */
 int main(void)
+char *b="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 {
-fprintf(stderr, "and that piece of art is useful\""
-        " - Dora Korpar, 2015-10-19\n");
+fprintf(stderr, b);
 return (1);
 }
