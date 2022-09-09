67afd8e12dde22017eaa8a183772bae53140f726
Osamede1997 <derrick_rita@hotmail.com>
2022-09-09 11:10:03 -0700

    message

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,13 +4,14 @@
 int main(void)
 {
 char x;
-char q;
 for (x = 'a'; x <= 'z'; ++x)
 {
 putchar(x);
 }
-for (q = 'A'; x <= 'Z'; ++q)
-putchar(q);
+for (x = 'A'; x <= 'Z'; ++x)
+{
+putchar(x);
 }
+putchar('\n');
 return (0);
 }
