34a3fd51323272cabc121c168e68484174b62b93
Osamede1997 <derrick_rita@hotmail.com>
2022-09-09 16:01:09 -0700

    message

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main entry point, it compares a random number to 5, 6, 0
+ * the return value of the main function is 0 if the code runs 
+ * successful, it returns a non zero value if the code 
+ * isnt successful
+ * Return: 0 if exited properly and non-zero if otherwise
+ */
+
 int main(void)
 {
 char x;
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * main entry point, it compares a random number to 5, 6, 0
+ * the return value of the main function is 0 if the code runs 
+ * successful, it returns a non zero value if the code 
+ * isnt successful
+ * Return: 0 if exited properly and non-zero if otherwise
+ */
+
 int main(void)
 {
 int n;
 for (n = 0; n < 10; ++n)
 {
-write(1, n, sizeof(n));
+write(1, &n, sizeof(n));
 }
 putchar("\n");
 return (0);
