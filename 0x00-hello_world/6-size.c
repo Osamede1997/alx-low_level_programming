6bc7bff39a2c56c2b3638ccf99abaf4d1629f2c3
Osamede1997 <derrick_rita@hotmail.com>
2022-09-08 17:07:46 -0700

    message

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-printf("Size of a char: %i\n", (int)sizeof(char));
-printf("Size of an int: %i\n", (int)sizeof(int));
-printf("Size of a long long int: %i\n", (int)sizeof(long long int));
-printf("Size of a float: %i\n", (int)sizeof(float));
+printf("Size of a char: %ibyte(s)\n", (int)sizeof(char));
+printf("Size of an int: %ibyte(s)\n", (int)sizeof(int));
+printf("Size of a long long int: %ibyte(s)\n", (int)sizeof(long long int));
+printf("Size of a float: %ibyte(s)\n", (int)sizeof(float));
 return (0);
 }
