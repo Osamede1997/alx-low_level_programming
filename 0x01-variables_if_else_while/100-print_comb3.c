fd44b5d926ded09bce1162a714d2a8764d63558b
Osamede1997 <derrick_rita@hotmail.com>
2022-09-12 12:33:18 -0700

    message

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,10 +8,16 @@
 int main(void)
 {
 int n;
-for (n = 0; n <= 99; n++)
+int e;
+int b;
+
+
+e = n/10 + '0';
+b = n%10 + '0';
+for (n = 0; n <= 89; n++)
 {
-putchar(n/10 + '0');
-putchar(n%10 + '0');
+putchar(e);
+putchar(b);
 putchar(',');
 putchar(' ');
 }
