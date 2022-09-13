2f807056d94ba6e28cd99a0d2edc42d2d403713a
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 14:29:48 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,19 @@
 
 int print_last_digit(int n)
 {
+if (n > 0)
+{
 
-n = n % 10;
+n = (n % 10);
+_putchar(n + '0');
+return (n);
 
+}
+else 
+{
+n = (-1 * n) % 10;
+_putchar(n + '0');
+return (n);
+}
 return (n);
 }
