964b4c7ce8c6aa160817c719f8ab4ba062d773d6
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 07:17:36 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check if alphabet is lowercase
+ * Return: Always 0.
+ */
+
+int _islower(int c);
+{
+int c;
+if (c >= 97 and c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
