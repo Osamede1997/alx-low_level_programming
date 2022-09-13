b81eaf63e62f659470084b79546838b81d4c4b22
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 14:51:29 -0700

    commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,20 +6,16 @@
  * Returns i - the last digit of the number
  */
 
-int print_last_digit(int n)
+int _abs(int n)
 {
 if (n > 0)
 {
-
-n = (n % 10);
-_putchar(n + '0');
 return (n);
 
 }
 else 
 {
-n = (-1 * n) % 10;
-_putchar(n + '0');
+n = -1 * n;
 return (n);
 }
 }
