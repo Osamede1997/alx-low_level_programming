f5163f75db56a37ed1b623c4b228bc2de8b95e52
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 14:11:22 -0700

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - checks if int is less
+ * @n : integer to be tested 
+ * Returns i - the last digit of the number
+ */
+
+int print_last_digit(int)
+{
+int n;
+int i;
+
+i = n % 10
+
+return (i);
+}
