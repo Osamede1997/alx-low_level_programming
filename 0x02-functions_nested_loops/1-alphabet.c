d2db821898271cd09fcac6758bb6b680bf9e5557
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 06:38:52 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,21 +5,16 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-
-print_alphabet();
-return (0);
 
 void print_alphabet(void)
 {
 char n;
 for (n = 'a'; n <= 'z'; n++)
 {
-_putchar(n)
+_putchar(n);
 if (n < 'z')
 {
-_putchar('\n')
+_putchar('\n');
 }
 }
 }
