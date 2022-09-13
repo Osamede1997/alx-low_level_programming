52f73a6be431262f42d8b58172336c931847de8f
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 07:23:48 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - check if alphabet is uppercase
+ * Return: Always 0.
+ */
+
+int _islower(int c);
+{
+int c;
+if (c >= 65  and c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
