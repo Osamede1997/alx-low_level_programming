28bfc5104e98b1802fd6337feb5c5d7d94eb63f2
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 16:04:16 -0700

    commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - Prints the last digit of an int
  * @n : integer to be tested
- * Return - the value of last digit of the number
+ * Return - return the value of last digit of the number
  */
 
 int print_last_digit(int n)
@@ -15,7 +15,7 @@ a = (n % 10);
 _putchar(a + '0');
 return (a);
 }
-else 
+else
 {
 a = (-1 * n) % 10;
 _putchar(a + '0');
