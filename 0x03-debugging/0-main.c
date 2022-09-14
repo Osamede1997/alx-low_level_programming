6a6d59460f0e89b95ea66f0976d86d62ea753fa0
Osamede1997 <derrick_rita@hotmail.com>
2022-09-14 07:07:57 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
+* main - test that prints if integer is positive or negative
 * Return: returns value 0 if success
 */
 
