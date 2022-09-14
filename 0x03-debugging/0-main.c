b08fe6098333c6ba413235ec488e30167bdc7d89
Osamede1997 <derrick_rita@hotmail.com>
2022-09-14 07:44:53 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
-* main - entry point test that prints if integer is positive or negative
-* Return: 0 if success
+* main - entry point, prints if integer is positive or negative
+* Return: value of 0 if success
 */
 
 int main(void)
