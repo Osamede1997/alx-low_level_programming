44167bac1546a6d454410fbcb10d8c12f84a64db
Osamede1997 <derrick_rita@hotmail.com>
2022-09-13 17:40:27 -0700

    commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * jack_bauer - prints out every minute of the day
+ * return - no return value
+ */
 
 
 void jack_bauer(void)
@@ -15,4 +19,5 @@ for (n = 0; n < 60; n++)
 printf("%i%i:%i%i\n", i / 10, i % 10, n / 10, n % 10);
 }
 }
+return;
 }
