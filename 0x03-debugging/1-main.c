3ca520f805d0017701161965617769440cfc935c
Osamede1997 <derrick_rita@hotmail.com>
2022-09-14 08:57:00 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - causes a loop, prints statements of loop
-* Return: value of 0 if success
+* main - causes an infinite loop
+* Return: 0
 */
 
 int main(void)
@@ -12,12 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/**
-* while (i < 10)
-* {
-* putchar(i);
-* }
-*/
+
+while (i < 10)
+{
+putchar(i);
+}
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
