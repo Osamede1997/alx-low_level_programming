9560df1c25f25a398c76afe53f1d025c5fd0cf16
Osamede1997 <derrick_rita@hotmail.com>
2022-09-15 13:07:57 -0700

    commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _isupper - check if alphabet is uppercase
- * @c : character to check the case
- * Return: always 0 or 1
+ *@c : character to check the case
+ * Return: Always 0 or 1
  */
 
 int _isupper(int c)
 {
-if (c >= 65  && c <= 90)
+if (c >= 'A'  && c <= 'Z')
 {
 return (1);
 }
