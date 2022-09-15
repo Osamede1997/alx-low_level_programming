6c812b2309f19eae8ac1a0fed0bb2c29631bb1f1
Osamede1997 <derrick_rita@hotmail.com>
2022-09-15 16:59:45 -0700

    message

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-int i;
+int i, j;
 
 if (n < 1)
 {
@@ -18,10 +18,13 @@ else
 {
 for (i = 0; i < n; i++)
 {
-_putchar('\\');
-_putchar('\\');
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
 }
+_putchar('\\');
 _putchar('\n');
+}
 }
 return;
 }
