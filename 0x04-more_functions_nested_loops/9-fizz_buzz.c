22211b104334f14592cce77f7ec73ca53e71e40b
Osamede1997 <derrick_rita@hotmail.com>
2022-09-15 16:35:27 -0700

    commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,30 +8,6 @@
 
 int main(void)
 {
-int i;
-for (i = 1; i < 101; i++)
-{
-if ((i % 3 == 0) && (i % 5 == 0))
-{
-printf("FizzBuzz");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else
-{
-printf("%i",i);
-}
-if (i != 100)
-{
-printf(" ")
-}
-}
-printf("\n")
+
 }
 
