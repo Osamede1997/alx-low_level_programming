8b523ff7ac0003468587cf067de42112ca05582c
Osamede1997 <derrick_rita@hotmail.com>
2022-09-15 12:59:33 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - evaluates the product of two ints
+ * @a : integer to be tested
+ * @b : integer to be tested
+ * Return: the product of a and b
+ */
+
+int mul(int a, int b)
+{
+int c = a * b;
+
+return (c);
+}
