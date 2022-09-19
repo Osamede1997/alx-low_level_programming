b397efbc32a473ab352960616666b26f50a73bf4
Osamede1997 <derrick_rita@hotmail.com>
2022-09-19 14:43:28 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints an array
+ * @a: parameter tested
+ * @n: parameter tested
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+{
+printf("%i", a[i]);
+if (i > 0 && i != n)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
