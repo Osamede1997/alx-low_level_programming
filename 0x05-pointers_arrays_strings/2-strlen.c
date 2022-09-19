68c9fa14662d21e8bdd765132aedaf59741feb8a
Osamede1997 <derrick_rita@hotmail.com>
2022-09-19 11:47:25 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen: lget length of string
+ * @s: parameter tested
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int len;
+len = 0;
+
+while (*s != 0)
+{
+s++;
+len++
+}
+return (len);
+}
