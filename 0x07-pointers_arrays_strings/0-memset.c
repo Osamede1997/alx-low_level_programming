5005d0ad77a1548c15eeeb8ad18ebb202799b858
Osamede1997 <derrick_rita@hotmail.com>
2022-09-26 15:19:07 -0700

    commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - join two strings
+ * @ds: character tested
+ * @b: character tested
+ * @n: character tested
+ * Return: pointer to s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; n > 0; i++, n--)
+{
+s[i] = b;
+}
+return (s);
+}
