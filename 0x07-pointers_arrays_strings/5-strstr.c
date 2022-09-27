47cc8dcd8a3e1619522cbb59b487fdaf7ca0c987
Osamede1997 <derrick_rita@hotmail.com>
2022-09-27 04:24:29 -0700

    commit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ int i, j;
 
 for (i = 0; haystack[i] >= '\0'; i++)
 {
-for (j = i; haystack[j] >= '\0' && needle [j - i] > '\0'; j++)
+for (j = i; haystack[j] > '\0' && needle [j - i] > '\0'; j++)
 {
 if (haystack[j] != needle[j - i])
 {
