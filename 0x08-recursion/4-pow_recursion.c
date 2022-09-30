71fa8b372eda3e2e0b063ac7b6b69f453a00eccf
Osamede1997 <derrick_rita@hotmail.com>
2022-09-29 18:07:30 -0700

    commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursions - returns x raised to power y
+ * @n: int tested
+ * Return: facorial
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+
+if (y == 0)
+{
+return (1);
+}
+
+return (x * _pow_recursion(x, y - 1));
+}
