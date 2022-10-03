39265d8c794acd5ecb208a9535017a6545556c7a
Osamede1997 <derrick_rita@hotmail.com>
2022-10-03 04:25:33 -0700

    commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument vector
+ * return: 0 if success
+ */
+
+
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
