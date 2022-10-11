afeb5bcced1a748a718ef65855f3d84a657d02c9
Osamede1997 <derrick_rita@hotmail.com>
2022-10-10 19:48:25 -0700

    commit

---
+++ 0x0E-structures_typedef/5-frree_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog
+ * @d: struct dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+free(d->owner);
+free(d->name);
+free(d);
+
+}
