12ba890af3548810342c06448f356b1bda0b18ab
Osamede1997 <derrick_rita@hotmail.com>
2022-10-10 19:29:49 -0700

    commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,12 @@
  * @owner: string
  */
 
-struct dog
+typedef struct dog
 {
     char *name;
     float age;
     char *owner;
-}
-/**
- * dog_t - Typdef for struct dog
- */
-
-typedef struct dog dog_t;
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
