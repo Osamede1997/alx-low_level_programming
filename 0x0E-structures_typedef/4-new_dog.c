aacbaca9de4049e13c511196077131a1c63ee089
Osamede1997 <derrick_rita@hotmail.com>
2022-10-11 15:42:48 -0700

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,9 +12,8 @@ int _strlen(char *s)
 int len;
 len = 0;
 
-while (*s != 0)
+while (s[len] != '\0')
 {
-s++;
 len++;
 }
 return (len);
@@ -50,34 +49,37 @@ return (dest);
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *dog;
+dog_t *doggo;
 int len1, len2;
 
 len1 = _strlen(name);
 len2 = _strlen(owner);
 
-dog = malloc(sizeof(dog_t));
+doggo = malloc(sizeof(dog_t));
 
-if (dog == NULL)
+if (doggo == NULL)
 {
 return (NULL);
 }
 
-dog->name = malloc(sizeof(char) * (len1 + 1));
-if (dog->name == NULL)
+doggo->name = malloc(sizeof(char) * (len1 + 1));
+if (doggo->name == NULL)
 {
+free(doggo);
 return (NULL);
 }
 
-dog->owner = malloc(sizeof(char) * (len2 + 1));
-if (dog->name == NULL)
+doggo->owner = malloc(sizeof(char) * (len2 + 1));
+if (doggo->name == NULL)
 {
+free(doggo);
+free(doggo->name);
 return (NULL);
 }
 
-_strcpy(dog->name, name);
-_strcpy(dog->owner, owner);
-dog->age = age;
+_strcpy(doggo->name, name);
+_strcpy(doggo->owner, owner);
+doggo->age = age;
 
-return (dog);
+return (doggo);
 }
