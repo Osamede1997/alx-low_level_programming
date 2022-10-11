0791067994153172c0665501424157f42b3ced82
Osamede1997 <derrick_rita@hotmail.com>
2022-10-10 19:37:45 -0700

    commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,18 +13,18 @@ printf("Name: (nil)\n");
 }
 else
 {
-printf("Name: \n", d->name);
+printf("Name: %s\n", d->name);
 }
 
-printf("Age: \n", d->age);
+printf("Age: %f\n", d->age);
 
 if (d->owner == NULL)
 {
-printf("Owner: (nil)\n")
+printf("Owner: (nil)\n");
 }
 else
 {
-printf("Owner: \n", d->owner);
+printf("Owner: %s\n", d->owner);
 }
 
 }
