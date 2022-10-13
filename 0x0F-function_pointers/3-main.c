8c8d8f586bc411f4343904be9f824a1eca783255
Osamede1997 <derrick_rita@hotmail.com>
2022-10-13 14:24:53 -0700

    commit

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+int num1, num2;
+int (*f)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+if (argv[2][1])
+{
+printf("Error\n");
+exit(99);
+}
+
+f = get_op_func(argv[2]);
+
+if (f == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+printf("%d\n", f(a, b));
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+
+/**
+ * op_add - function that computes addition
+ * @a: int
+ * @b: int
+ * return: int sum
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - function that computes subtreaction
+ * @a: int
+ * @b: int
+ * return: int sub
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - function that computes multiplication
+ * @a: int
+ * @b: int
+ * return: int mul
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - function that computes addition
+ * @a: int
+ * @b: int
+ * return: int div
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - function that computes modulation
+ * @a: int
+ * @b: int
+ * return: int mod
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
