cd9f8ee8aee89ac12ecee75981e9fb8039d48dd0
Osamede1997 <derrick_rita@hotmail.com>
2022-10-21 03:51:29 -0700

    git commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,24 +11,25 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t node = 0;
+
+size_t node = 0;
 
 
-    while (h)
-    {
-    if (h->str == NULL)
-    {
-        printf("[0] (nil)\n");
-    }
+while (h)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+}
 
-    else
-    {
-        printf("[%d] %s\n", h->len, h->str);
-    }
-    
-    node++;
-    h = h->next;
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+}
 
-    }
-    return (node);
+node++;
+h = h->next;
+
+}
+return (node);
 }
