19ae202069c557af011163c82da030ece4154900
Osamede1997 <derrick_rita@hotmail.com>
2022-10-31 18:17:49 -0700

    commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
+ * append_text_to_file - Appends text at the end of a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int file3, w, len = 0;
 
