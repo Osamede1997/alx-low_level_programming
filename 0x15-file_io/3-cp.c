4019843f9d8cde63e2a03f49db299c54634b443e
Osamede1997 <derrick_rita@hotmail.com>
2022-11-01 05:56:54 -0700

    commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+
+int main (int argc, char *argv[])
+{
+    int file1, file2, n;
+    char buf[1024];
+
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    file1 = open(argv[1], O_RDONLY);
+
+    if (file1 == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    file2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+    n = read(file1, buf, 1024);
+    while (n > 0)
+    {
+        if (write(file2, buf, n) != n || file2 == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+
+    }
+    if (n == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    if (close(file1) < 0)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
+        exit(100);
+    }
+    if (close(file2) < 0)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+        exit(100);
+    }
+    return (0);
+    
+}
